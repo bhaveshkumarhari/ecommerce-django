613999f59dce0832bb53c15f5e766243936cc661
Bhaveshkumar Hari <bhaveshkumar.hari@gmail.com>
2020-02-20 14:37:37 +0000

    Worked within home-page and converted FBV to CBV.

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import item_list, product, checkout
+from .views import HomeView, ItemDetailView, checkout
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name="item-list"),
-    path('product/', product, name="product"),
+    path('', HomeView.as_view(), name="home"),
+    path('product/<slug>/', ItemDetailView.as_view(), name="product"),
     path('checkout/', checkout, name="checkout")
 ]
--- core/views.py
+++ core/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Item
 
-# Create your views here.
 
-def item_list(request):
+class HomeView(ListView):
+    model = Item
+    template_name = "home-page.html"
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product-page.html"
+
+"""
+Function based view for homepage
+
+def home(request):
     context = {
         'items': Item.objects.all()
     }
     return render(request, 'home-page.html', context)
-
-def product(request):
-    return render(request, 'product-page.html')
+"""
 
 def checkout(request):
     return render(request, 'checkout-page.html')
