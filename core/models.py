a37919f163d66bde83995d6759b26cdabd0d266a
Bhaveshkumar Hari <bhaveshkumar.hari@gmail.com>
2020-02-20 19:07:51 +0000

    Worked with adding items to cart

--- core/models.py
+++ core/models.py
@@ -21,6 +21,7 @@ class Item(models.Model):
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     label = models.CharField(choices=LABEL_CHOICES, max_length=1)
     slug = models.SlugField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
@@ -29,12 +30,21 @@ class Item(models.Model):
         return reverse("core:product", kwargs={
             'slug': self.slug
         })
+    
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
 
 class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                            on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
-        return self.title
+        return f"{self.quantity} of {self.item.title}"
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Item
+from django.shortcuts import redirect
+from django.utils import timezone
+from .models import Item, OrderItem, Order
 
+def checkout(request):
+    return render(request, 'checkout-page.html')
 
 class HomeView(ListView):
     model = Item
@@ -10,6 +14,30 @@ class HomeView(ListView):
 class ItemDetailView(DetailView):
     model = Item
     template_name = "product-page.html"
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug) # get specific item with slug
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+        )
+    order_qs = Order.objects.filter(user=request.user, ordered=False) # filter order which is not completed by the specific user
+
+    if order_qs.exists():
+        order = order_qs[0] # grab the order from the order_qs
+        # check if the order item in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
+    else:
+        ordered_date = timezone.now() # get current date
+        order = Order.objects.create(user=request.user, ordered_date=ordered_date) # Create Order model instance with specific user and ordered date
+        order.items.add(order_item) # Then add order_item to that model instance
+    return redirect("core:product",slug=slug)
+
 
 """
 Function based view for homepage
@@ -21,5 +49,3 @@ def home(request):
     return render(request, 'home-page.html', context)
 """
 
-def checkout(request):
-    return render(request, 'checkout-page.html')
