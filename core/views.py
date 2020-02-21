5ec749bac412051a2d9ae1158f6c54fb83d0a211
Bhaveshkumar Hari <bhaveshkumar.hari@gmail.com>
2020-02-21 14:40:36 +0000

    Worked with removing items from cart

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import redirect
@@ -17,27 +18,64 @@ class ItemDetailView(DetailView):
 
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug) # get specific item with slug
+    # If item is not in OrderItem model then add item else get the item
     order_item, created = OrderItem.objects.get_or_create(
         item=item,
         user=request.user,
         ordered=False
         )
-    order_qs = Order.objects.filter(user=request.user, ordered=False) # filter order which is not completed by the specific user
+
+    # filter Order model which is not ordered yet by the specific user
+    order_qs = Order.objects.filter(user=request.user, ordered=False) 
 
     if order_qs.exists():
         order = order_qs[0] # grab the order from the order_qs
-        # check if the order item in the order
+
+        # check if an item exists in Order model with slug
         if order.items.filter(item__slug=item.slug).exists():
-            order_item.quantity += 1
-            order_item.save()
+            order_item.quantity += 1 # Increase quantity in OrderItem model if there is an item exists
+            order_item.save() # Save OrderItem model
+            messages.info(request, "This item quantity was updated.")
         else:
-            order.items.add(order_item)
+            order.items.add(order_item) # Add item to Order model if item does not exist in OrderItem.
+            messages.info(request, "This item was added to your cart.")
+            return redirect("core:product",slug=slug)
     else:
         ordered_date = timezone.now() # get current date
         order = Order.objects.create(user=request.user, ordered_date=ordered_date) # Create Order model instance with specific user and ordered date
         order.items.add(order_item) # Then add order_item to that model instance
-    return redirect("core:product",slug=slug)
+        messages.info(request, "This item was added to your cart.")
+        return redirect("core:product",slug=slug)
 
+
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug) # get specific item with slug
+
+    # filter Order model which is not ordered yet by the specific user
+    order_qs = Order.objects.filter(
+        user=request.user, 
+        ordered=False
+    ) 
+
+    if order_qs.exists():
+        order = order_qs[0] # grab the order from the order_qs
+
+        # check if an item exists in Order model with slug
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item) # Remove item if an item exists in Order model
+            messages.info(request, "This item was removed from your cart.")
+            return redirect("core:product",slug=slug)
+        else:
+            messages.info(request, "This item was not in your cart.")
+            return redirect("core:product",slug=slug)
+    else:
+        messages.info(request, "You do not have an active order.")
+        return redirect("core:product",slug=slug)
 
 """
 Function based view for homepage
